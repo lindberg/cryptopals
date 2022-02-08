674fdb9f8c496dc7b2cfa965ec46726d42b11fb3
lindberg <etf2@live.se>
2022-02-08 23:37:18 +0100

    Update

--- set1/challenge-1.py
+++ set1/challenge-1.py
@@ -1,9 +1,8 @@
 import base64
 
-def hexstring2base64(hex_string):
-    byte_array = bytes.fromhex(hex_string)
-    return base64.b64encode(byte_array).decode("utf-8")
-
 hex_string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 
-print(hexstring2base64(hex_string))
+byte_array = bytes.fromhex(hex_string)
+base64_string = base64.b64encode(byte_array).decode("utf-8")
+
+print(base64_string)
