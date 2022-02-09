c86cb4db0ef3297584e2fda3d723609786685dba
lindberg <etf2@live.se>
2022-02-09 23:24:53 +0100

    print update

--- set1/challenge_8.py
+++ set1/challenge_8.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     for idx, ciphertext in enumerate(ciphertexts):
         blocks = [ciphertext[i:i+16] for i in range(0, len(ciphertext), 16)]
         if len(blocks) != len(set(blocks)):
-            print(f"Ciphertext at line {idx} was encrypted using ECB!")
+            print(f"Ciphertext at line {idx} was encrypted using ECB mode!")
