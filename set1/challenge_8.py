c488495df6e4bc78035cd98209a87430a331a55a
lindberg <etf2@live.se>
2022-02-09 23:21:07 +0100

    challenge 8

---
+++ set1/challenge_8.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    ciphertexts = [bytes.fromhex(ciphertext) for ciphertext in open("8.txt").read().split()]
+
+    for idx, ciphertext in enumerate(ciphertexts):
+        blocks = [ciphertext[i:i+16] for i in range(0, len(ciphertext), 16)]
+        if len(blocks) != len(set(blocks)):
+            print(f"Ciphertext at line {idx} was encrypted using ECB!")
